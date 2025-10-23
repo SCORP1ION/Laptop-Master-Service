b6c52ef194cfd4f23c9ffdf39932d59508265255
SCORP1ION <SCORP1ION@gmail.com>
2025-10-23 15:18:03 -0600

    Bloque de desplazamiento horizontal

--- App.js
+++ App.js
@@ -11,7 +11,8 @@ const Stack = createStackNavigator();
 function MyStack(){
   return(
     // retorna una pila para que valla al la vista VisLogin
-    <Stack.Navigator>
+    // ggestureEnabled: false. Bloquea el desplazamiento horizontal de las vistas
+    <Stack.Navigator screenOptions={{gestureEnabled: false}}>
         <Stack.Screen name='VLogin' component={VisLogin} options={{title: 'Iniciar Sesion', headerShown: false}} />
         <Stack.Screen name='Vinicio' component={VisInicio} options={{headerShown: false}}/>
         <Stack.Screen name='ViRegis' component={VisRegister} options={{title: 'Registrarse', headerShown: false}}/>
